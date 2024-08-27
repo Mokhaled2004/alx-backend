97107db86ffb9b571163310941568a87ca1618c2
Mokhaled2004 <mokhaled2004@gmail.com>
2024-08-27 16:40:28 +0000

    Solo Leveling

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env python3
 """A Basic Flask app with internationalization support.
 """
@@ -44,4 +43,3 @@ def get_index() -> str:
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
